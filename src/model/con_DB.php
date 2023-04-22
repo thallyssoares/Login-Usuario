379b939e011067d56bc33e192816d3da544cf2c1
Thallys <antoniothallys2017@gmail.com>
2023-04-22 15:43:21 -0700

    commit inicial e feito a classe de conexão com o banco de dados

---
+++ src/model/con_DB.php
@@ -0,0 +1,79 @@
+<?php 
+
+class Conexao_DB{
+    private $nome;
+    private $email;
+    private $senha;
+
+    private function __construct($n,$s){
+        $this->nome = $n;
+        $this->senha = $s;
+    }
+    
+    public function Login(){
+        try {
+            $pdo = new PDO("mysql:dbname=loginuser;host=localhost", "root", "");
+            $res = $pdo->prepare("SELECT * FROM usuario WHERE nome=:n");
+            $res->bindValue(":n", $this->nome);
+            $res->execute();
+
+            $resultado = $res->fetch(PDO::FETCH_ASSOC);
+
+            if(count($resultado) > 0){
+                if($this->senha == $resultado["senha"]){
+                    session_start();
+
+                    $SESSION["name"] = $resultado["nome"];
+                    $SESSION["id"] = $resultado["id"];
+                    $SESSION["logado"] = true;
+
+                    header("location:../../index.php");
+                }
+
+            } else{
+                echo "<p>Erro ao logar na conta. Tente novamente.</p>";
+            }
+
+        } catch (PDOException $e) {
+    
+            echo "<p>Houve um erro ao conectar com o Banco de Dados, esse foi o erro gerado: </p>" . $e->getMessage();
+        } catch (Exception $e){
+            echo "<p>Houve um erro, esse foi o erro gerado: </p>". $e->getMessage();
+        }
+        
+
+    }
+    
+    public function Cadastrar($e){
+        try {
+            $this->email = $e;
+            $pdo = new PDO("mysql:dbname=loginuser;host=localhost", "root", "");
+            $validacao = $pdo->prepare("SELECT * FROM usuario WHERE nome=:n");
+            $validacao->bindValue(":n", $this->nome);
+            $retorno = $validacao->execute();
+
+            if($retorno){
+                echo "<p>O usuario que você tentou cadastrar já existe.</p>";
+            } else{
+                $res = $pdo->prepare("INSERT INTO usuario(nome, email, senha) VALUES (:n, :e, :s)");
+                $res->bindValue(":n", $this->nome);
+                $res->bindValue(":e", $this->email);
+                $res->bindValue(":s", $this->senha);
+                $res->execute();
+
+                echo "<p>Cadastro feito com sucesso. Volte a página de login pra entrar na conta.</p>";
+            }
+            
+        } catch (PDOException $e) {
+            echo "<p>Houve um erro ao conectar com o Banco de Dados, esse foi o erro gerado: </p>" . $e->getMessage();
+        } catch (Exception $e){
+            echo "<p>Houve um erro, esse foi o erro gerado: </p>" . $e->getMessage();
+        }
+    }
+    
+    
+
+};
+
+
+?>
