d78f4edd40c77a632b6361cdf2c6b5f331295420
Thallys <antoniothallys2017@gmail.com>
2023-04-29 15:36:49 -0700

    feito a função de mostrar os clientes.

--- index.php
+++ index.php
@@ -18,59 +18,34 @@
 <body>
     <header>
         <h1>Painel Administrativo</h1>
-        <nav>
-            <a href="index.php?cmd=cad">Cadastrar Cliente</a>
-            <a href="index.php?cmd=atu">Atualizar Dados</a>
-            <a href="index.php?cmd=delete">Deletar Cliente</a>
-            <a href="index.php?cmd=sair">Sair da Conta</a>
-        </nav>
     </header>
     <main>
-        <?php 
-            if(!empty($_GET["cmd"])){
-                
-                $cmd = $_GET["cmd"];
-                
-                switch ($cmd) {
-                    case "cad":
-                        
-                        echo '<form action="" method="post">
-                            <label for="nome">Nome:</label>
-                            <input type="text" name="nome" id="idNome"><br>
+        <div class="cadCliente">     
+            <form action="" method="post">
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome" id="idNome"><br>
 
-                            <label for="email">Email</label>
-                            <input type="email" name="email" id="idEmail"><br>
+                <label for="email">Email</label>
+                <input type="email" name="email" id="idEmail"><br>
 
-                            <label for="tel">Telefone</label>
-                            <input type="text" name="tel" id="idTel"><br>
+                <label for="tel">Telefone</label>
+                <input type="text" name="tel" id="idTel"><br>
 
-                            <input type="submit" value="Cadastrar">
-                        </form>';
-                        
-                        if(!empty($_POST["nome"])){
-                            $nome = $_POST["nome"];
-                            $email = $_POST["email"];
-                            $tel = $_POST["tel"];
-                            $db = new Conexao_DB_Cliente;
-                            $db->cad_Cliente($nome, $email, $tel, $_SESSION["id"]);
-                        } else {
-                            echo "<p>Preencha todos os dados corretamente.</p>";
-                        }
-                        
-                        break;
-
-                    case "atu":
-                        echo "<p>$cmd</p>";
-                        break;
-                    case "delete":
-                        echo "<p>$cmd</p>";
-                        break;
-                    default:
-                        echo "<p>$cmd</p>";
-                        break;
+                <input type="submit" value="Cadastrar">
+            </form>;
+            <?php 
+                if(!empty($_POST["nome"])){
+                    $db = new Conexao_DB_Cliente();
+                    $db->cad_Cliente($_POST["nome"], $_POST["email"], $_POST["tel"], $_SESSION["id"]);
                 }
-            }
-        ?>
+            
+            ?>
+        </div>
+        <div class="mostCliente">
+                <?php 
+                    $db->most_Cliente($_SESSION["id"]);
+                ?>
+        </div>
     </main>
 </body>
 </html>
--- src/model/con_DB_Cliente.php
+++ src/model/con_DB_Cliente.php
@@ -37,6 +37,18 @@ class Conexao_DB_Cliente{
         }
         
     }
+    public function most_Cliente($id){
+        $res = $this->pdo->prepare("SELECT c.nome, c.email, c.telefone FROM clientes c JOIN usuario u ON u.id=c.idusuario WHERE c.idusuario=:id ORDER BY c.nome;");
+        $res->bindValue(":id", $id);
+        $res->execute();
+        $resultado = $res->fetchAll(PDO::FETCH_ASSOC);
+
+        
+        foreach ($resultado as $res) {
+            echo $res["nome"] ." ". $res["email"] ." ". $res["telefone"] ."<br>";
+        }
+        
+    }
 }
 
 ?>
